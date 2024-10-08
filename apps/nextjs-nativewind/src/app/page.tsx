7469cc21c963bb3cc22b116a386d9cc7def3830e
Zach Nugent <mrzachnugent@gmail.com>
2024-10-08 11:08:23 -0400

    feat: add nextjs-nativewind app

---
+++ apps/nextjs-nativewind/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-12 justify-center items-center py-24 px-4">
+      <p className="animate-in fade-in-0 duration-1000">Nextjs NativeWind</p>
+    </main>
+  );
+}
