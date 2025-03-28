14deef16ba9e2d7423428d05a14de6fdc3ab17da
Zach Nugent <mrzachnugent@gmail.com>
2025-03-28 14:53:53 -0400

    feat: add merge props to utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export { EmptyGestureResponderEvent } from './empty-gesture-responder-event';
+export { mergeProps } from './merge-props';
 export { renderPressableChildren } from './render-pressable-children';
 export { flattenStyle } from './style/flatten-style';
 export { rnStyleToWebStyle } from './style/rn-style-to-web-style';
