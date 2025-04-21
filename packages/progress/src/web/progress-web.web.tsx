99c29e80a7f8920a72ddf15b47852659d2bac4e9
Paul Kilmurray <paul@kilbot.com>
2025-04-21 13:50:48 +0200

    use withRNPrimitives util

--- packages/progress/src/web/progress-web.web.tsx
+++ packages/progress/src/web/progress-web.web.tsx
@@ -1,12 +1,7 @@
 import { Progress, ProgressIndicator } from '@radix-ui/react-progress';
-import type { RootProps, IndicatorProps } from './types';
+import { withRNPrimitives } from '@rn-primitives/utils';
 
-function Root(props: RootProps) {
-  return <Progress data-rn-primitives='view' {...props} />;
-}
-
-function Indicator(props: IndicatorProps) {
-  return <ProgressIndicator data-rn-primitives='view' {...props} />;
-}
+const Root = withRNPrimitives(Progress, 'view');
+const Indicator = withRNPrimitives(ProgressIndicator, 'view');
 
 export { Root, Indicator };
