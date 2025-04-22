375d8abc952ae7e247e95581b4dd45c7906e735e
Paul Kilmurray <paul@kilbot.com>
2025-04-22 23:42:32 +0200

    Update universal toggle web component

--- packages/toggle/src/universal/toggle.web.tsx
+++ packages/toggle/src/universal/toggle.web.tsx
@@ -2,12 +2,15 @@ import { Pressable } from '@rn-primitives/core';
 import { Root as RootWeb } from '../web';
 import type { RootProps } from './types';
 
-function Root({ native: _native, web, ...props }: RootProps) {
-  return (
-    <RootWeb asChild>
-      <Pressable web={{ as: 'button', ...web }} {...props} />
-    </RootWeb>
-  );
+function Root({ native: _native, web, style, ...props }: RootProps) {
+  if (style) {
+    return (
+      <Pressable style={style} asChild>
+        <RootWeb {...props} {...(web as any)} />
+      </Pressable>
+    );
+  }
+  return <RootWeb {...props} {...(web as any)} />;
 }
 
 export { Root };
