43bf853f1f74cae89db7704db6d9cca6fc998345
Zach Nugent <mrzachnugent@gmail.com>
2026-03-15 14:06:50 -0400

    feat: add useComposedRefs hook for managing multiple refs

---
+++ packages/hooks/src/use-compose-refs.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+export function useComposedRefs<T>(
+    ...refs: Array<React.Ref<T> | undefined>
+): React.RefCallback<T> {
+    return React.useCallback(composeRefs(...refs), refs);
+}
+
+function setRef<T>(ref: React.Ref<T> | undefined, value: T | null): (() => void) | void {
+    if (typeof ref === 'function') {
+        const cleanup = ref(value);
+
+        if (typeof cleanup === 'function') {
+            return cleanup;
+        }
+
+        return;
+    }
+
+    if (ref != null) {
+        ref.current = value;
+        return () => {
+            ref.current = null;
+        };
+    }
+}
+
+function composeRefs<T>(
+    ...refs: Array<React.Ref<T> | undefined>
+): React.RefCallback<T> {
+    let cleanups: Array<() => void> = [];
+
+    return (node) => {
+        cleanups.forEach((cleanup) => cleanup());
+        cleanups = [];
+
+        if (node == null) {
+            refs.forEach((ref) => {
+                if (typeof ref === 'function') {
+                    ref(null);
+                } else if (ref != null) {
+                    ref.current = null;
+                }
+            });
+            return;
+        }
+
+        cleanups = refs
+            .map((ref) => setRef(ref, node))
+            .filter((cleanup): cleanup is () => void => cleanup != null);
+    };
+}
